de3c3d3b1128ca0e3995da96a39e3d2d42b89423
Jungyong lee <jungyonglee@Jungyongs-MacBook-Air.local>
2022-09-27 13:51:12 +0900

    save to md file

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # neovim-ds
+
+## requirements
+
+### lua
+- https://github.com/rcarriga/nvim-notify
+
+### python
+- pynvim
+- matplotlib
+
+### mac
+brew install cairo
--- python/stream.py
+++ python/stream.py
@@ -1,28 +1,30 @@
 """
-example
-
-cell 1
-from IPython import get_ipython
-get_ipython().register_magics(CustomMagics)
-
-cell 2
-%%tee output
-for _ in range(0,10):
-  time.sleep(1)
-  print(56)
 
 """
 
 
+from pynvim import attach
 from io import StringIO
 import sys
+import string
 
+import re
 from IPython.core import magic_arguments
 from IPython.core.magic import Magics, cell_magic, magics_class
 from IPython.utils.capture import CapturedIO
 
+def string_head(input, max_line=3, max_letter=40):
+  lines = input.split("\n")
+  lines = [line[:max_letter] + ".." if len(line) > max_letter else line for line in lines]
+  # lines = [line.replace("'", "\\\'") for line in lines]
+  # lines = [re.sub("'", "\\\'", line) for line in lines]
+  # lines = [line.replace('"', "'") for line in lines]
+  # lines = [re.sub('"', "\\\"", line) for line in lines]
 
-class Tee(StringIO):
+  # lines = [line[:min(len(line), max_letter)] for line in lines]
+  return '\n'.join(lines[:min(len(lines), max_line)])
+
+class NeoDSStream(StringIO):
   def __init__(self, initial_value='', newline='\n', stream=None, fio=None):
     self.stream = stream
     self.fio = fio
@@ -37,17 +39,17 @@ class Tee(StringIO):
     super().write(data)
 
 
-class capture_and_print_output(object):
+class NeoDSCaptureOutput(object):
   stdout = True
   stderr = True
   display = True
   
-  def __init__(self, stdout=True, stderr=True, display=True):
+  def __init__(self, stdout=True, stderr=True, display=True, buf=''):
     self.stdout = stdout
     self.stderr = stderr
     self.display = display
     self.shell = None
-    self.fio = open("hihi.md", "a")
+    self.fio = open(buf, "a")
   
   def __enter__(self):
     from IPython.core.getipython import get_ipython
@@ -65,9 +67,9 @@ class capture_and_print_output(object):
     
     stdout = stderr = outputs = None
     if self.stdout:
-      stdout = sys.stdout = Tee(stream=sys.stdout, fio=self.fio)
+      stdout = sys.stdout = NeoDSStream(stream=sys.stdout, fio=self.fio)
     if self.stderr:
-      stderr = sys.stderr = Tee(stream=sys.stderr, fio=self.fio)
+      stderr = sys.stderr = NeoDSStream(stream=sys.stderr, fio=self.fio)
     if self.display:
       self.save_display_pub = self.shell.display_pub
       self.shell.display_pub = CapturingDisplayPublisher()
@@ -88,26 +90,52 @@ class capture_and_print_output(object):
 
 
 @magics_class
-class CustomMagics(Magics):
+class DsMagic(Magics):
   @magic_arguments.magic_arguments()
   @magic_arguments.argument('output', type=str, default='', nargs='?')
   @magic_arguments.argument('--no-stderr', action='store_true')
   @magic_arguments.argument('--no-stdout', action='store_true')
   @magic_arguments.argument('--no-display', action='store_true')
+  @magic_arguments.argument('--stream-buffer', default='')
+  @magic_arguments.argument('--bufnr', default=None)
+  @magic_arguments.argument('--vpath', default=None)
+
   @cell_magic
-  def tee(self, line, cell):
-    with open("hihi.md", "a") as fio:
-      md_cell = f"""
-```python
-{cell}
-```
-"""
-      fio.write(md_cell)
-    args = magic_arguments.parse_argstring(self.tee, line)
+  def neods(self, line, cell):
+    args = magic_arguments.parse_argstring(self.neods, line)
     out = not args.no_stdout
     err = not args.no_stderr
     disp = not args.no_display
-    with capture_and_print_output(out, err, disp) as io:
-      self.shell.run_cell(cell)
+    buf = args.stream_buffer
+    bufnr = args.bufnr
+    vpath = args.vpath
+
+    vim = attach('socket', path=vpath)
+    vim.command("lua require('notify')('started')")
+
+    with open(buf + "_code", 'r') as input_file:
+      input_code = re.sub(chr(0),'\n',input_file.read())
+
+    with open(buf, "a") as fio:
+      md_cell = f"""
+---
+```python
+{input_code.strip()}
+```
+
+"""
+      fio.write(md_cell)
+
+    with NeoDSCaptureOutput(out, err, disp, buf) as io:
+      self.shell.run_cell(input_code)
     if args.output:
       self.shell.user_ns[args.output] = io
+
+    vim.command("e")
+    vim.exec_lua(f"""
+local nprint = [[
+{string_head(input_code.strip())}
+]]
+require('notify')(nprint, 'info', {{title='Done'}})
+    """)
+
